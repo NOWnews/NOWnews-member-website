bc0b9f589d63f6b541d2c73f9e84175ff29fda3f
esbb48 <esbb48@gmail.com>
2017-07-31 03:06:08 +0800

    ✨ Feat: Update member menu ui

--- hocs/standardLayout.js
+++ hocs/standardLayout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Router from 'next/router'
 import { connect } from 'react-redux'
 import Head from 'next/head'
-import { Container, Header, Image, Menu } from 'semantic-ui-react';
+import { Sidebar, Segment, Icon, Container, Header, Image, Menu, Button } from 'semantic-ui-react';
 
 const standardLayoutHoc = (Page, activeItem, title) => {
   class standardLayout extends React.Component {
@@ -16,24 +16,81 @@ const standardLayoutHoc = (Page, activeItem, title) => {
         currentUrl: ctx.pathname
       }
     }
+  state = { visible: false }
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
 
     render () {
+      const { visible } = this.state
       return (
         <div>
-          <Menu stackable inverted>
-            <Menu.Item>
-              NOWnews 會員系統
-            </Menu.Item>
-            <Menu.Item active={activeItem === 'me'}>
-              <a href='/member/me'>會員資料</a>
-            </Menu.Item>
-            <Menu.Item position='right'>
-              <a href='/auth/logout'>登出</a>
-            </Menu.Item>
-          </Menu>
-          <Container className='standard-layout'>
-            <Page {...this.props} />
-          </Container>
+          <style>{`
+              #root {
+                height: 100vh;
+              }
+              #root > .grid {
+                border-radius: 0;
+              }
+              .padding0 {
+                padding: 0 !important;
+              }
+          `}</style>
+          <Sidebar.Pushable>
+            <Sidebar
+              as={Menu}
+              animation='overlay'
+              width='thin'
+              direction='right'
+              visible={visible}
+              icon='labeled'
+              vertical
+              inverted>
+              <Menu.Item name='user' active={activeItem === 'me'}>
+                <a href='/member/me'>
+                  <Icon name='user' />會員資料
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a href='/auth/logout'>
+                  <Icon name='log out' />登出
+                </a>
+              </Menu.Item>
+              <Menu.Item position='right'>
+                <label onClick={this.toggleVisibility}>
+                  <Icon name='angle double right' />返回
+                </label>
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher>
+              <div id='root'>
+                <Menu inverted className='grid'>
+                  <div className='padding0 row computer only'>
+                    <Menu.Item>
+                      NOWnews 會員系統
+                    </Menu.Item>
+                    <Menu.Item  active={activeItem === 'me'}>
+                      <a href='/member/me'>會員資料</a>
+                    </Menu.Item>
+                    <Menu.Item position='right'>
+                      <a href='/auth/logout'>登出</a>
+                    </Menu.Item>
+                  </div>
+                  <div className='padding0 row mobile only'>
+                    <Menu.Item>
+                      NOWnews 會員系統
+                    </Menu.Item>
+                    <Menu.Item position='right'>
+                      <label onClick={this.toggleVisibility}>
+                        <Icon name='content' />
+                      </label>
+                    </Menu.Item>
+                  </div>
+                </Menu>
+                <Container className='standard-layout'>
+                  <Page {...this.props} />
+                </Container>
+              </div>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
         </div>
       )
     }
