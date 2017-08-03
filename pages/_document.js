be561be6ad557f596eb035f9ddc24a15de46d55f
esbb48 <esbb48@gmail.com>
2017-08-03 13:57:09 +0800

    🐛 Fix: Fix toator css problem

--- pages/_document.js
+++ pages/_document.js
@@ -15,8 +15,8 @@ class MyDocument extends Document {
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <script src="https://unpkg.com/axios/dist/axios.min.js" />
+          <link rel="stylesheet" type="text/css" href="http://diegoddox.github.io/react-redux-toastr/7.1/react-redux-toastr.min.css" />
           <link rel="stylesheet" type="text/css" href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css' />
-          <link rel="stylesheet" type="text/css" href="http://diegoddox.github.io/react-redux-toastr/7.0/react-redux-toastr.min.css" />
         </Head>
         <body>
           <Main />
